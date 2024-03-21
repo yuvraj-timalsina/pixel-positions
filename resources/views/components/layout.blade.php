4ae9e572660ff06e92318620cfe6f6380c3832b4
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2024-03-21 18:58:43 +0545

    Update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Lara11</title>
+    <title>LaraJobs</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="h-full">
