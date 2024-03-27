ba4d9acbc77b11f9e2b7bd8171bb69cac693b36e
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2024-03-27 09:51:57 +0545

    relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,16 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     use HasFactory;
+
     protected $table = 'job_listings';
 
     protected $fillable = [
         'title',
-        'salary'
+        'salary',
     ];
 
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
