a14f9ed8669854312cef92772b434251d9d0f80a
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2024-04-14 08:19:17 +0545

    edit job page

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,6 +2,9 @@
     <x-slot:heading>
         Job Details
     </x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <p>This job pays : {{ $job['salary'] }} per year.</p>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
+    <p>This job pays : {{ $job->salary }} per year.</p>
+    <p class='mt-6'>
+        <x-button href="{{ route('jobs.edit', $job->id) }}">Edit Job</x-button>
+    </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,26 @@ Route::get('/about', static function () {
     return view('about');
 })->name('about');
 
+// index
 Route::get('/jobs', static function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(5);
 
     return view('jobs.index', ['jobs' => $jobs]);
 })->name('jobs');
 
+// create
 Route::get('/jobs/create', static function () {
     return view('jobs.create');
 })->name('jobs.create');
 
+// show
 Route::get('/jobs/{id}', static function ($id) {
-    $job = Job::query()->find($id);
+    $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 })->name('jobs.show');
 
+// store
 Route::post('jobs', static function () {
     request()?->validate([
         'title' => ['required', 'min:3'],
@@ -41,6 +45,23 @@ Route::post('jobs', static function () {
 
     return redirect()->route('jobs');
 })->name('jobs.store');
+
+// edit
+Route::get('/jobs/{id}/edit', static function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+})->name('jobs.edit');
+
+// update
+Route::patch('/jobs/{id}', static function ($id) {
+    //
+});
+
+// destroy
+Route::delete('/jobs/{id}', static function ($id) {
+    //
+})->name('jobs.destroy');
 
 Route::get('/contact', static function () {
     return view('contact');
