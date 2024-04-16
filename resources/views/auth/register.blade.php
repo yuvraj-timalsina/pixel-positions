659e68f19f51332508f1f94b981ed5a43f50cbcc
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2024-04-16 09:16:24 +0545

    ✨ Add login and register forms.

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,64 @@
+<x-layout>
+    <x-slot:heading>
+        Register
+    </x-slot:heading>
+
+    <form method="POST" action="{{ route('register-user') }}">
+        @csrf
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <x-form-field>
+                        <x-form-label for="first_name">First Name</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="first_name" id="first_name" required/>
+                            <x-form-error name="first_name"/>
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="last_name">Last Name</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="last_name" id="last_name" required/>
+                            <x-form-error name="last_name"/>
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="email" type="email">Email</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="email" id="email" required/>
+                            <x-form-error name="email"/>
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="password" type="password">Password</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="password" id="password" required/>
+                            <x-form-error name="password"/>
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="password_confirmation" type="password">Confirm Password</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="password_confirmation" id="password_confirmation" required/>
+                            <x-form-error name="password_confirmation"/>
+                        </div>
+                    </x-form-field>
+
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a href="{{route('home')}}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <x-form-button>Register</x-form-button>
+        </div>
+    </form>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('', 'home')->name('home');
@@ -8,3 +10,9 @@ Route::view('about', 'about')->name('about');
 Route::view('contact', 'contact')->name('contact');
 
 Route::resource('jobs', JobController::class);
+
+Route::get('register', [RegisterUserController::class, 'create'])->name('register');
+Route::post('register', [RegisterUserController::class, 'store'])->name('register-user');
+
+Route::get('login', [SessionController::class, 'create'])->name('login');
+Route::post('login', [SessionController::class, 'store'])->name('login-user');
