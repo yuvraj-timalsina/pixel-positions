599c9a38fc6d92920b2d89f0afd2c21dadc4a7da
Yuvraj Timalsina <yuvrajtimalsina@outlook.com>
2024-05-24 00:13:02 +0545

    wip

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -9,9 +9,6 @@ use App\Models\Tag;
 
 class JobController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $jobs = Job::all()->groupBy('featured');
@@ -23,49 +20,31 @@ class JobController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreJobRequest $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Job $job)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Job $job)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateJobRequest $request, Job $job)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Job $job)
     {
         //
